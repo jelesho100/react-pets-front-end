03b65e8b5234e388f28b7d1a4ed9b47780346063
jelesho <jelesho100@gmail.com>
2025-08-14 12:18:24 -0400

    crud front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-// src/App.jsx
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index();
+        // Don't forget to pass the error object to the new Error
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+        setPets(fetchedPets);
+      } catch (err) {
+        // Log the error object
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+  return (
+    <div>
+      <h1>Pet List</h1>
+      <div>
+        {!props.pets.length ? (
+          <h2>No Pets Yet!</h2>
+        ) : (
+          <ul>
+            {props.pets.map((pet) => (
+              <li key={pet._id}>{pet.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+
 
 export default App;
-
---
+++ src/components/PetList/PetList.jsx
@@ -0,0 +1,15 @@
+// src/components/PetList/PetList.jsx
+
+const PetList = (props) => {
+  // Let's ensure we have data to work with before adding functionality!
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Pet List</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default PetList;
